4385c6b2ac88289f5380f5d3b556e76ad4a01578
AaronChu87 <121922893+AaronChu87@users.noreply.github.com>
2023-01-05 16:36:44 +0800

    Delete helperCullRecentMapPoints.m

--- helperCullRecentMapPoints.m
+++
@@ -1,9 +0,0 @@
-function [mapPointSet, directionAndDepth, mapPointsIdx] = ...
-    helperCullRecentMapPoints(mapPointSet, directionAndDepth, mapPointsIdx, newPointIdx, rgbdMapPointsIndices)
-outlierIdx = setdiff([newPointIdx; rgbdMapPointsIndices], mapPointsIdx);
-if ~isempty(outlierIdx)
-    mapPointSet   = removeWorldPoints(mapPointSet, outlierIdx);
-    directionAndDepth = remove(directionAndDepth, outlierIdx);
-    mapPointsIdx  = mapPointsIdx - arrayfun(@(x) nnz(x>outlierIdx), mapPointsIdx);
-end
-end
